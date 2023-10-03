8ae035badfb18382910b79ecfa34412f46da9e4c
cheiru94 <cheiru94@gmail.com>
2023-10-04 01:35:42 +0900

    study of react

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+import Example from './contextTest/Example'
+import './App.css'
+// import Reducer from './Reducer'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <Reducer/> */}
+      <Example/>
+    </>
+  )
+}
+
+export default App
