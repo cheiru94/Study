69422a2acae3fec97156527fb5f026ba5471923e
cheiru94 <cheiru94@gmail.com>
2023-10-13 23:26:21 +0900

    mainPage

--- laravel/laravelStudy/resources/views/welcome.blade.php
+++ laravel/laravelStudy/resources/views/welcome.blade.php
@@ -8,6 +8,11 @@
 
     </head>
     <body>
-        재일이
+        <h1>curl -s https://laravel.build/<span style=color:blue>프로젝트 이름</span> | bash</h1>
+        <h1>./vender/bin/sail up : 도커 원래 실행하는 명렁</h1>
+        <h1>./vender/bin/sail up -d : 백그라운드에서 도커 실행하는 명렁</h1>
+        <h1>./vender/bin/sail stop : 도커 중지 명렁</h1>
+
+        <h1>sail up : 도커 실행</h1>   ↑ alias 지정 후 사용가능
     </body>
 </html>
