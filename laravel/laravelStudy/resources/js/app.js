864d3088eefbecb0a28a5e46b6fd4aa98c851156
cheiru94 <cheiru94@gmail.com>
2023-10-13 23:24:33 +0900

    add Inertia

--- laravel/laravelStudy/resources/js/app.js
+++ laravel/laravelStudy/resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+/* 스캐폴딩  Inertia , react 가져오기 */
+// import { InertiaApp } from '@inertiajs/inertia-react' // 🍑　Inertia 
+// import React from 'react'　// 🍑　Inertia 
+// import { render } from 'react-dom'
+
+// const el = document.getElementById('app')
+
+// render(
+//   <InertiaApp
+//     initialPage={JSON.parse(el.dataset.page)}
+//     resolveComponent={name => require(`./Pages/${name}`).default}
+//   />,
+//   el
+// )
