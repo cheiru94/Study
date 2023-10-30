bfb39b4c455c59406f258cb38887aa4cdfe4fe30
cheiru94 <cheiru94@gmail.com>
2023-10-31 01:12:36 +0900

    ::create는 모델 인스턴스를 데이터 베이스에 저장 시킨다

--- laravel/laravel_start/app/Http/Controllers/ContactFormController.php
+++ laravel/laravel_start/app/Http/Controllers/ContactFormController.php
@@ -34,7 +34,9 @@ class ContactFormController extends Controller
         // dd($request , $request-> name); // 리퀘스트로 받은 내용들을 검사해서 출력 
 
         // 변수는 따로 지정해 주지 않아도 된다. => 여기에 작성된 내용은 ContactForm 모델의 $fillable에 지정되어 있어야 한다. 
-        ContactForm::create([
+        ContactForm::create([ // ContactForm 모델 인스턴스를 데이터베이스에 저장
+
+            // 사용자로 부터 입력받은 데이터를 뽑아 와서, 이를 기반으로 ContactForm 레코드를 생성한다.
             'name' => $request->name,
             'title' => $request->title,
             'email' => $request->email,
@@ -43,7 +45,7 @@ class ContactFormController extends Controller
             'age' => $request->age,
             'contact' => $request->contact,
         ]);
-        return to_route('contacts.index'); // to_route 메서드로 리다이렉트 할 수 있다.
+        return to_route('contacts.index'); // to_route 메서드로 리다이렉트 할 수 있다.   
     }
 
     /**
