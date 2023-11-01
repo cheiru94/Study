30b1c6e7e8a809226a45a22b54f7f652d53036e5
cheiru94 <cheiru94@gmail.com>
2023-11-01 12:03:57 +0900

    DB에서 원하는 내용 가져오기  -> select 메소드

--- laravel/laravel_start/app/Http/Controllers/ContactFormController.php
+++ laravel/laravel_start/app/Http/Controllers/ContactFormController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ContactForm;
+use App\Models\ContactForm; // ContactForm 불러와 사용한다고 use 해주기
 use Illuminate\Http\Request;
 
 class ContactFormController extends Controller
@@ -10,11 +10,11 @@ class ContactFormController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index()  // contacts폴더 안의 index.blade.php 파일로 내용을 전송한다
     {
-        //
-        $contacts = ContactForm::select('id','name','title','created_at')->get();
-        return view('contacts.index',compact('contacts')); // 이렇게 적으면 위의 변수 $contacts를 전달할 수 있다.  
+        // get() 메서드를 호출하지 않은 경우, 실제 쿼리는 데이터베이스에서 실행되지 않는다.
+        $contacts = ContactForm::select('id','name','title','created_at')->get(); // get() 메서드는 데이터베이스에서 쿼리를 실행하고, 결과를 컬렉션 형태로 반환
+        return view('contacts.index',compact('contacts')); // 이렇게 적으면 위의 변수 $contacts를 전달할 수 있다. 🩵 compact함수는 변수 이름을 입력으로 받아, 그 이름을 가진 변수의 이름과 값을 가진 배열을 생성해 반환
     }
 
     /**
@@ -32,12 +32,13 @@ class ContactFormController extends Controller
     public function store(Request $request)
     {
         // dd($request); // $request 만으로 폼에 입력한 정보가 들어있다.  -> request 안에  parameters에 폼에 입력한 정보들이 들어있음
-        dd($request , $request-> name); // 리퀘스트로 받은 내용들을 검사해서 출력 
+        // dd($request , $request-> name); // 리퀘스트로 받은 내용들을 검사해서 출력 
+
 
         // 변수는 따로 지정해 주지 않아도 된다. => 여기에 작성된 내용은 ContactForm 모델의 $fillable에 지정되어 있어야 한다. 
-        ContactForm::create([ // ContactForm 모델 인스턴스를 데이터베이스에 저장
+/* $contactForm = */ContactForm::create([ // ContactForm 모델 인스턴스를 데이터베이스에 저장 -> ContactForm 을 불러온다고 위에 use로 작성해주어야 한다 
 
-            // 사용자로 부터 입력받은 데이터를 뽑아 와서, 이를 기반으로 ContactForm 레코드를 생성한다.
+            // form 태그에서 사용자로부터 입력받은 데이터를 뽑아 와서, 이를 기반으로 ContactForm 레코드를 생성한다.
             'name' => $request->name,
             'title' => $request->title,
             'email' => $request->email,
@@ -46,7 +47,8 @@ class ContactFormController extends Controller
             'age' => $request->age,
             'contact' => $request->contact,
         ]);
-        return to_route('contacts.index'); // to_route 메서드로 리다이렉트 할 수 있다.   
+        // 지정해 놓은 라우트 이름으로 리다이렉트
+        return to_route('contacts.index'); // 저장한 이후에는 리다이렉트를 해주는게 일반적이다.  to_route 메서드로 리다이렉트 할 수 있다.   
     }
 
     /**
