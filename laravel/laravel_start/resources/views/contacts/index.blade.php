1cc9a61dcece29dab5c887109d28a8e04cde8303
cheiru94 <cheiru94@gmail.com>
2023-11-01 09:52:52 +0900

    신규등록에 a태그로 경로 추가

--- laravel/laravel_start/resources/views/contacts/index.blade.php
+++ laravel/laravel_start/resources/views/contacts/index.blade.php
@@ -10,7 +10,15 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                      index<br>
+                     {{-- web.php에서 라우터에 이름을 앞에는 contacts. 하고 /contacts/create 경로의 name에는 create라고 지정해 놓았었음 
+                        prefix('contacts') 라도고 지정해 두었다 -> url 경로 앞에는 무조건 contacts붙게.--}}
                      <a href="{{route('contacts.create')}}" class="text-blue-500">신규등록</a>
+                     @foreach ($contacts as $contact)
+                         {{$contact->id}}
+                         {{$contact->name}}
+                         {{$contact->title}}
+                         {{$contact->created_at}}
+                     @endforeach
                 </div>
             </div>
         </div>
