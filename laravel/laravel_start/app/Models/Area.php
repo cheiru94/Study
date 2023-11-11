16a2fe7b593e4e83bbc50d01ff3e6e3ca11ab420
cheiru94 <cheiru94@gmail.com>
2023-11-12 00:23:52 +0900

    1:다 관계 정의

--- laravel/laravel_start/app/Models/Area.php
+++ laravel/laravel_start/app/Models/Area.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Area extends Model
 {
     use HasFactory;
+
+    public function shops (){
+      // $this 변수는 현재 객체의 인스턴스를 참조
+      // 현재 객체'란 $this 키워드가 속해있는 메소드가 호출되는 객체
+      return $this->hasMany(Shop::class); 
+                                          
+    }
 }
--- laravel/laravel_start/app/Models/Shop.php
+++ laravel/laravel_start/app/Models/Shop.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+   public function area(){
+      return $this->belongsTo(Area::class);
+   }
 }
