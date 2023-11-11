6ba2815939bf35f9429a3c7c789c535f6c3ff45f
cheiru94 <cheiru94@gmail.com>
2023-11-11 22:36:08 +0900

    shops 마이그레이션

--- laravel/laravel_start/database/migrations/2023_11_11_115943_create_shops_table.php
+++ laravel/laravel_start/database/migrations/2023_11_11_115943_create_shops_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
+            $table->string('name'); 
+            $table->foreignId('area_id'); // 외래키 : 부모-모델명_id
             $table->timestamps();
         });
     }
