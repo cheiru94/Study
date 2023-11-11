edd8876209356b17457ac7b250d96de06e6a53d7
cheiru94 <cheiru94@gmail.com>
2023-11-11 17:03:18 +0900

    factory는 model과 이름에 기반한 연계를 함

--- laravel/laravel_start/database/seeders/DatabaseSeeder.php
+++ laravel/laravel_start/database/seeders/DatabaseSeeder.php
@@ -13,17 +13,23 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-      // 이렇게 해줘야 추가 된다.
+      /* 🟢 seeder 추가  =>  이렇게 해줘야 추가 된다 */
       $this->call([
         TestSeeder::class,
         UserSeeder::class,
       ]);
       
-        \App\Models\ContactForm::factory(100)->create();
+      /* 🟡 factory 추가  */
+      // 라라벨의 팩토리 시스템은 모델과 팩토리 클래스 간에 이름에 기반한 자동 해석 기능을 가지고 있다
+      // 🟡 ContactFormFactory는 🟡ContactForm 모델에 대한 팩토리로 자동으로 인식된다.
+      // 그래서 아래처럼 작성하면 라라벨은 ContactForm 모델에 대해 팩토리를 호출하고, 
+      // 그 팩토리에서 100개의 더미 데이터를 생성하라는 의미
+      \App\Models\ContactForm::factory(100)->create();
+    
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+      // \App\Models\User::factory()->create([
+      //     'name' => 'Test User',
+      //     'email' => 'test@example.com',
+      // ]);
     }
 }
