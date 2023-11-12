feb0a6696ce1a778f67b5d251397e50fbe2eeaee
cheiru94 <cheiru94@gmail.com>
2023-11-12 19:23:26 +0900

    sort_no 주석 수정

--- laravel/laravel_start/database/migrations/2023_11_11_115715_create_areas_table.php
+++ laravel/laravel_start/database/migrations/2023_11_11_115715_create_areas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('areas', function (Blueprint $table) {
             $table->id();
             $table->string('name'); // 지역 이름
-            $table->integer('sort_no'); // sort number
+            $table->integer('sort_no'); // sort number : 사용자가 지정하는 정수 값 (순서를 나타내거나 사용자 정의 순서 등)
             $table->timestamps();
         });
     }
