6c1958dd53019caac923968ecac8bd91563867ce
cheiru94 <cheiru94@gmail.com>
2023-11-14 01:46:16 +0900

    명명된 슬롯

--- laravel/laravel_intermediate/resources/views/test/component-test1.blade.php
+++ laravel/laravel_intermediate/resources/views/test/component-test1.blade.php
@@ -1,4 +1,5 @@
 
 <x-tests.app>
-컴포넌트 테스트 1
+  <x-slot name='header'>헤더 1</x-slot>
+  컴포넌트 테스트 1
 </x-tests.app>
--- laravel/laravel_intermediate/resources/views/test/component-test2.blade.php
+++ laravel/laravel_intermediate/resources/views/test/component-test2.blade.php
@@ -1,5 +1,6 @@
 
 <x-tests.app>
+  <x-slot name='header'>헤더 1</x-slot>
   컴포넌트 테스트 2
 </x-tests.app>
 
