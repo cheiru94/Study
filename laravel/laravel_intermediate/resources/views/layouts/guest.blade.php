b5f82a74e216d09fb1fa2a3fa251e2fc563d825c
cheiru94 <cheiru94@gmail.com>
2023-11-22 19:20:27 +0900

    라라벨 믹스의 경우 주석 추가

--- laravel/laravel_intermediate/resources/views/layouts/guest.blade.php
+++ laravel/laravel_intermediate/resources/views/layouts/guest.blade.php
@@ -11,8 +11,17 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
+        <!-- Scripts 라라벨 믹스를 사용시 이렇게 vite를 사용하면 에러가 발생해 css가 다 깨진다.  -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- 라라벨 믹스를 사용할 경우 아래와 같이 2가지를 적용시켜 주어야 한다. -->
+        {{-- <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script> --}}
+
+
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
