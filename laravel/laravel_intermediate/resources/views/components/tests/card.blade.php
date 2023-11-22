7b229a5a6f721358a8cdbd6698843e146bb7aa86
cheiru94 <cheiru94@gmail.com>
2023-11-23 00:58:49 +0900

    속성으로 설정

---
+++ laravel/laravel_intermediate/resources/views/components/tests/card.blade.php
@@ -0,0 +1,13 @@
+
+<div class="border-2 shadow-md w-1/4 p-2">
+  <div>{{$title}}</div>
+  <div>화상</div>
+  <div>{{$content}}</div>
+</div>
+
+
+
+
+
+
+
--- laravel/laravel_intermediate/resources/views/tests/component-test1.blade.php
+++ laravel/laravel_intermediate/resources/views/tests/component-test1.blade.php
@@ -2,4 +2,13 @@
 <x-tests.app> {{-- blade.php는 안적어도 된다 --}}
   <x-slot name='header'>헤더 1</x-slot> {{-- 🟢 명명된 slot 사용 => name에 tests/app.blade.php에서 작성했던 변수명 기입  --}}
   コンポーネントテスト 1 {{-- app.blade.php 파일 안의 $slot 이라고 지정해 놓은 부분에 들어가는 내용 --}}
+
+
+  <x-tests.card title="타이틀 프롭" content="콘텐트 프롭"/> {{-- $slot이 없어 안에 아무 것도 넣을 것이 없으며 이렇게도 가능--}}
+
 </x-tests.app>
+
+
+
+
+
