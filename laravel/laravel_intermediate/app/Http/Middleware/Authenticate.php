31bf5362811387b20e5b11243487716e38b790d2
cheiru94 <cheiru94@gmail.com>
2023-11-25 16:56:00 +0900

    Authenticate.php 작성

--- laravel/laravel_intermediate/app/Http/Middleware/Authenticate.php
+++ laravel/laravel_intermediate/app/Http/Middleware/Authenticate.php
@@ -4,14 +4,31 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class Authenticate extends Middleware
 {
+
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';  // RouteServiceProvider.php에서 as로 설정해 두었다.
+    protected $admin_route = 'admin.login';
+
+
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * 사용자가 인증되지 않은 경우 리디렉션해야 하는 경로를 가져옵니다.
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!$request->expectsJson()) {
+            if (Route::is('owner.*')) {
+                return route($this->owner_route);
+            } else if (Route::is('admin.*')) {
+                return route($this->admin_route);
+            } else {
+                return route($this->user_route);
+            }
+        }
+        // return $request->expectsJson() ? null : Route::is('owner.*') ? route($this->owner_route) : null;; // 인증되지 않은 유저는 전부 , login화면으로 리다이렉션 된다.
+
     }
 }
