e7566ed31df3d0b20d9458a466149ef94ebb88d1
cheiru94 <cheiru94@gmail.com>
2024-01-15 02:10:02 +0900

    feat : 토큰으로 유저 인증 처리

--- express-auth/index.js
+++ express-auth/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const user = { name: username };
+  const user = { name: username }; // payload
 
   /* JWT 토큰 생성 : Payload + SecretText */
   const accessToken = jwt.sign(user, secretText);
@@ -32,6 +32,22 @@ app.post("/login", (req, res) => {
 app.get("/posts", (req, res) => {
   res.json(posts);
 });
+
+function authMiddileware(req, res, next) {
+  // 토큰을 request header에서 가져오기
+  const authHeader = req.headers["authorization"]; // Bearer ojojojo.oldfwlelfw.ofjwefj 까지 추출
+  console.log("authHeader: ", authHeader);
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log("token: ", token);
+
+  if (token === null) return res.sendStatus(401);
+
+  jwt.verify(token, secretText, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user; // 이렇게 user 값을 넣으면 다음 미들웨어에서 user 값을 사용할 수 있다.
+    next();
+  });
+}
 
 const port = 4000;
 app.listen(port, () => {
